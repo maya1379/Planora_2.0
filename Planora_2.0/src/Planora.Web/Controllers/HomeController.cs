7cf8708852475e193e597d06a41d5316c63e2b4c
maya1379 <edumail220@gmail.com>
2026-04-27 09:00:34 +0300

    test fail

--- Planora_2.0/src/Planora.Web/Controllers/HomeController.cs
+++ Planora_2.0/src/Planora.Web/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ public class HomeController : Controller
     {
         if (User.Identity == null || !User.Identity.IsAuthenticated)
         {
-            return View("Landing");
+            return View("123");
         }
 
         var model = new List<ScheduleEntryDto>();
